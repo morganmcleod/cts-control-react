c5f7e890e607e657cf39921d89d9332d61e390b5
Morgan McLeod <mmcleod@nrao.edu>
2022-09-14 11:40:24 -0400

    Added CartridgeTemps component.  Talking to back end. Chrome launch config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+const axios = require('axios').default
+axios.defaults.baseURL = 'http://localhost:8000';
+
+class CartridgeTemps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp0: 0,
+      temp1: 0,
+      temp2: 0,
+      temp3: 0,
+      temp4: 0,
+      temp5: 0
+    }
+  }
+  render() {
+    return (<div>
+      temp0: {this.state.temp0}<br/>
+      temp1: {this.state.temp1}<br/>
+    </div>);
+  }
+  componentDidMount() {
+    axios.get(`/cca/tempsensors`)
+      .then(res => {
+        const temps = res.data;
+        this.setState({ 
+          temp0: temps.temp0,
+          temp1: temps.temp1,
+          temp2: temps.temp2,
+          temp3: temps.temp3,
+          temp4: temps.temp4,
+          temp5: temps.temp5
+         });
+      })
+  }
+}
+
 
 function App() {
   return (
@@ -17,6 +55,7 @@ function App() {
         >
           Learn React
         </a>
+        <CartridgeTemps />
       </header>
     </div>
   );
