c948241f011676b7840ee1debce920040086cc0a
morganmcleod <mmcleod@nrao.edu>
2023-08-17 17:34:07 -0400

    New raster ws logic wip

--- src/Measure/BeamScanner/Graph.js
+++ src/Measure/BeamScanner/Graph.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useEffect } from "react";
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { useDispatch, useSelector } from 'react-redux'
 import { setPosition } from "../../Hardware/BeamScanner/MotorControlSlice";
@@ -11,6 +11,7 @@ import axios from "axios";
 export default function BeamScannerGraph(props) {
   // Redux store interface
   const position = useSelector((state) => state.MotorControl.position);
+  const haveRasters = useSelector((state) => state.BeamScanner.rasters);
   const amplitudePlot = useSelector((state) => state.BeamScanner.amplitudePlot);
   const measSpec = useSelector((state) => state.BeamScanner.measurementSpec)
   const dispatch = useDispatch();
@@ -22,10 +23,10 @@ export default function BeamScannerGraph(props) {
     lastMessage: posMessage 
   } = useWebSocket("ws://localhost:8000/beamscan/position_ws", options);
     
-  // const { 
-  //   readyState: rasterReady,
-  //   lastMessage: rasterMessage 
-  // } = useWebSocket("ws://localhost:8000/beamscan/rasters_ws", options);
+  const { 
+    readyState: rasterReady,
+    lastMessage: rasterMessage 
+  } = useWebSocket("ws://localhost:8000/beamscan/rasters_ws", options);
 
   useEffect(() => {
     if (posReady === ReadyState.OPEN) {
@@ -39,58 +40,45 @@ export default function BeamScannerGraph(props) {
       }
     }
   }, [posReady, posMessage, dispatch]);
-
-  // useEffect(() => {
-  //   if (rasterReady === ReadyState.OPEN) {
-  //     if (rasterMessage !== null) {
-  //       try {
-  //         const rasters = JSON.parse(rasterMessage.data);
-  //         if (rasters.startIndex === 0)
-  //           dispatch(resetRasters());
-  //         for (const raster of rasters.rasters)
-  //           dispatch(addRaster(raster));
-  //       } catch (err) {
-  //         console.log(err);
-  //       }
-  //     }
-  //   }
-  // }, [rasterReady, rasterMessage, dispatch]);
- 
-  const startIndex = useRef(0);
-  const timer = useRef(0);
-
-  // Only fetch data when mounted
-  const isMounted = useRef(false);
-
-  const fetch = useCallback(() => {
-    axios.get('/beamscan/rasters', { params: { startIndex: startIndex.current }})
-    .then(res => {
-      if (res.data.rasters.length) {
-        if (res.data.startIndex === 0) {        
-          dispatch(resetRasters());
-        }
-        for (const raster of res.data.rasters)
-          dispatch(addRaster(raster));
-        startIndex.current = res.data.startIndex + res.data.rasters.length;        
-      }
-      timer.current = setTimeout(() => {fetch()}, props.interval ?? 4000);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }, [dispatch, props.interval, startIndex]);
-  
-  // Fetch on first render:
+   
   useEffect(() => {
-    isMounted.current = true;
-    fetch();
-    return () => { 
-      isMounted.current = false; 
-      if (timer.current) {
-        clearTimeout(timer.current);
-        timer.current = 0;
-      }};
-  }, [fetch]);
+    if (rasterReady === ReadyState.OPEN) {
+      if (rasterMessage !== null) {
+        // The websocket only sends the latest raster
+        try {
+          const rasters = JSON.parse(rasterMessage.data);
+          // If the result is empty or is the first raster, reset our copy:
+          if (rasters.startIndex === 0)
+            dispatch(resetRasters());
+            
+          // If we are missing some of the previous ones,
+          if (haveRasters.length < rasters.startIndex) {
+            // Request raters from index 0:
+            axios.get('/beamscan/rasters', { params: { first: 0, last: rasters.startIndex - 1 }})
+            .then(res => {
+              // Clear our copy:
+              dispatch(resetRasters());
+              // Add the result of the request:
+              for (const raster of res.data.rasters)
+                dispatch(addRaster(raster));
+              // Add the latest from the websocket:
+              for (const raster of rasters.rasters)
+                dispatch(addRaster(raster));
+            })
+            .catch(error => {
+              console.log(error);
+            });
+          } else {
+            // We're up to date so just add the new one:
+            for (const raster of rasters.rasters)
+              dispatch(addRaster(raster));        
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  }, [rasterReady, rasterMessage, dispatch, haveRasters.length]);
 
   return (
     <Plot 
